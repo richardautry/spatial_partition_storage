7d8a0e4ec703986a988d1cd5f29e51343fd526cc
Richard Autry <autry.richard@gmail.com>
2022-07-09 08:59:25 -0400

    Added blog code

--- blog_code.py
+++ blog_code.py
@@ -5,3 +5,23 @@ line_points = [
     (3, 0),
     (4, 0)
 ]
+
+line_a = None
+#  from point (0, 5) to point (33, 51)
+# x lookup table
+{
+    0: [line_a],
+    10: [line_a],
+    20: [line_a],
+    30: [line_a]
+}
+
+# y lookup table
+{
+    0: [line_a],
+    10: [line_a],
+    20: [line_a],
+    30: [line_a],
+    40: [line_a],
+    50: [line_a]
+}
